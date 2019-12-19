f24e8ea840b432174d859d25c5cd7f52dbd370f8
Robert Merrill <robgmerrill@gmail.com>
2019-12-19 14:10:06 -0800

    Fetch bitcoin average API and display corresponding message

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+var bitcoinPriceYesterday;
+var priceChange;
+var todaysQuote;
+
 var nakabotoQuote = document.querySelector('h1');
 console.log(nakabotoQuote);
 
@@ -8,12 +12,26 @@ var randomNakabotoQuotes = [
   "Satoshi Nakaboto: 'The bubble has burst!"
 ];
 
-function randomNakabotoQuote() {
-  var todaysQuote =
-    randomNakabotoQuotes[
-      Math.floor(Math.random() * randomNakabotoQuotes.length)
-    ];
+async function displayNakabotoBanner() {
+  let fetchResult = fetch(
+    'https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD'
+  );
+  let response = await fetchResult;
+  let data = await response.json();
+  console.log('data');
+  console.log(data.changes.price.day);
+  priceChange = data.changes.price.day;
+  if (priceChange > 2000) {
+    todaysQuote =
+      'Satoshi Nakaboto: We are heading to the moon! Bitcoin maximalism is real';
+  } else if (priceChange > 1000) {
+    todaysQuote = "Satoshi Nakaboto: It's great day to have some Bitcoin";
+  } else if (priceChange > 0) {
+    todaysQuote = "Satoshi Nakaboto: It's great day to have some Bitcoin";
+  } else {
+    todaysQuote = `Satoshi Nakaboto: Rough day to own some Bitcoin. The price is down ${priceChange} dollars!`;
+  }
   nakabotoQuote.textContent = todaysQuote;
 }
 
-randomNakabotoQuote();
+displayNakabotoBanner();
